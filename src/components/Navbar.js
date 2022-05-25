96e082b75ac21abbad28c91d19b2edec974458b7
j1mp-dev <tiago.quaresma.t0101129@edu.atec.pt>
2022-05-25 14:15:52 +0100

    Routing & Navbar
    Added project structure
    Added app routing structure
    Added navbar component
    Added styles logic for navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/components/Navbar.css"
+
+function Navbar() {
+    return ( 
+        <div class="navbar">
+            <h1>CVJUICER</h1>
+        </div>
+     );
+}
+
+export default Navbar;
